d0dbc6f9cb566edaebef94c9d6307ed5a4e3dae8
beasiteri <siteribea@gmail.com>
2022-03-12 09:42:21 +0100

    Create ingredient model

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,15 @@
+/*
+We can use this one or a shorthand see below with only constructor
+export class Ingredient {
+  public name: string;
+  public amount: number;
+
+  constructor(name: string, amount: number) {
+    this.name = name;
+    this.amount = amount;
+  }
+} */
+
+export class Ingredient {
+  constructor(public name: string, public amount: number) {}
+}
